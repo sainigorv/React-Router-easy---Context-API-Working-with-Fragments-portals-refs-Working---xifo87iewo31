0540e51bd64229f38e1c7f95a862fca398177c35
sainigorv <sainigorv341@gmail.com>
2022-12-23 19:11:57 +0530

    updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 
 export const NavBar = () =>{
     return(
+        <nav>
+            <ul>
+            <NavLink to="/" className = "index-link">
+            <li>Index</li>
+            </NavLink>
+            <NavLink to="/home" className = "home-link">
+            <li>Home</li>
+            </NavLink>
 
+            </ul>
+        </nav>
     )
 }
